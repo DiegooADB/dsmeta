44005178e5afafd2c2ebe99fb2f7118a2f3607cf
Diego <diegoaragao465@gmail.com>
2022-07-12 15:41:58 -0300

    Create controllers for Seller

---
+++ backend/src/main/java/me/diego/dsmeta/domain/Product.java
@@ -0,0 +1,25 @@
+package me.diego.dsmeta.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    private LocalDateTime createdAt;
+
+    private boolean itSold;
+}
---
+++ backend/src/main/java/me/diego/dsmeta/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package me.diego.dsmeta.repositories;
+
+import me.diego.dsmeta.domain.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+}
