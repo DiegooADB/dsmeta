f9c257778d6ed8ee610a33072106cf1775839986
Diego <diegoaragao465@gmail.com>
2022-07-14 19:56:33 -0300

    Twilio SMS

--- backend/src/main/java/me/diego/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/me/diego/dsmeta/controllers/SaleController.java
@@ -3,16 +3,14 @@ package me.diego.dsmeta.controllers;
 import lombok.RequiredArgsConstructor;
 import me.diego.dsmeta.entities.Sale;
 import me.diego.dsmeta.services.SaleService;
+import me.diego.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/sales")
@@ -21,10 +19,18 @@ public class SaleController {
     @Autowired
     private final SaleService saleService;
 
+    @Autowired
+    private final SmsService smsService;
+
     @GetMapping
     public ResponseEntity<Page<Sale>> findAll(@RequestParam(value = "minDate", defaultValue = "") String minDate,
                                               @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
                                               @PageableDefault Pageable pageable) {
         return ResponseEntity.status(HttpStatus.FOUND).body(saleService.findAll(minDate, maxDate, pageable));
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
---
+++ backend/src/main/java/me/diego/dsmeta/services/SmsService.java
@@ -0,0 +1,50 @@
+package me.diego.dsmeta.services;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import me.diego.dsmeta.entities.Sale;
+import me.diego.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Service
+public class SmsService {
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public void sendSms(Long saleId) {
+
+        Sale sale = saleRepository.findById(saleId).orElseThrow(() ->
+                new HttpClientErrorException(HttpStatus.BAD_REQUEST, " User not found"));
+
+        String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+        String msg = "Vendendor %s foi destaque em %s com um total de R$ %s"
+                .formatted(sale.getSellerName(), date, String.format("%.2f", sale.getAmount()));
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
+    }
+}
